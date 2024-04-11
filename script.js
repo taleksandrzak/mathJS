048b60eefa2716397e3d4e051adcd103eef12f1e
Tomasz Aleksandrzak <t.aleksandrzak@o2.pl>
2024-04-11 20:33:58 +0200

    add operators and range by user

--- script.js
+++ script.js
@@ -2,27 +2,61 @@ const addChkBox = document.getElementById("add");
 const subChkBox = document.getElementById("sub");
 const multChkBox = document.getElementById("mult");
 const diviChkBox = document.getElementById("divi");
-const ChkBoxs1 = [addChkBox, subChkBox, multChkBox, diviChkBox];
+const choosenOperatorsBox = [addChkBox, subChkBox, multChkBox, diviChkBox];
 
 const n10Rad = document.getElementById("n10");
 const n100Rad = document.getElementById("n100");
 const n1000Rad = document.getElementById("n1000");
 const n10000Rad = document.getElementById("n10000");
-const ChkBoxs2 = [n10Rad, n100Rad, n1000Rad, n10000Rad];
+const choosenRangeRad = [n10Rad, n100Rad, n1000Rad, n10000Rad];
 
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
 const task = document.getElementById("task");
+
+const randNumb = function (n) {
+  console.log(Math.trunc(Math.random() * n) + 1);
+};
+const randOpert = function (m) {
+  return Math.trunc(Math.random() * m);
+};
+
+let n, m;
+
+const choosenOperators = [];
+
+const allOperators = ["+", "-", "*", "/"];
+
+const OPMAP = {
+  "*": (n1, n2) => n1 * n2,
+  "/": (n1, n2) => n1 / n2,
+  "+": (n1, n2) => n1 + n2,
+  "-": (n1, n2) => n1 - n2,
+};
 
 btn1.addEventListener("click", (e) => {
   e.preventDefault();
-  reslChkBocx = ChkBoxs1.map((el) => el.checked);
-  console.log(reslChkBocx);
+  const checkedOperators = choosenOperatorsBox.map((box) => box.checked);
+  checkedOperators.forEach((box, i) => {
+    if (box) {
+      choosenOperators.push(allOperators[i]);
+    }
+  });
+  m = choosenOperators.length;
 });
 
 btn2.addEventListener("click", (e) => {
   e.preventDefault();
-  const reslChkBocx = ChkBoxs2.map((el) => el.checked);
-  console.log(reslChkBocx);
-  task.innerText = "2 + 2 = ";
+  const choosenRange = choosenRangeRad.forEach((rad) => {
+    if (rad.checked) {
+      n = Number(rad.id.slice(1));
+    }
+  });
+});
+
+btn3.addEventListener("click", () => {
+  console.log(choosenOperators);
+  console.log(n);
+  console.log(m);
 });
